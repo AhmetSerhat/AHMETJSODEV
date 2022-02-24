e7b0cf59a002dec5ebbe0bca58487f758916ec71
AhmetSerhat <97557680+AhmetSerhat@users.noreply.github.com>
2022-02-24 13:32:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,21 +9,21 @@ function yer(){
     
     if(ulke==1){
     document.body.style.backgroundColor="yellowgreen"
-    sonuc.innerHTML="Nüfus: 38,01 milyon "
+    sonuc.innerHTML="Kanada, Kuzey Amerika'da bir ülkedir. On eyalet ve üç bölgeden oluşan ülke, Büyük Okyanus'tan Atlas Okyanusu'na ve kuzeyde Arktik Okyanusu'na kadar 9,98 milyon km²'lik bir alanı kaplar. Bu özelliğiyle yüzölçümü bakımından dünyanın Rusya'dan sonra en büyük 2. ülkesidir."
     sonuc.classList.add("CA");
     document.getElementById("logo").setAttribute("src","canada.png")
    }
     
    else if(ulke==2){
     document.body.style.backgroundColor="burlywood"
-    sonuc.innerHTML="Nüfus: 329,5 milyon"
+    sonuc.innerHTML="Amerika Birleşik Devletleri, Birleşik Devletler ya da resmî olmayan ismiyle Amerika, orta Kuzey Amerika'da, Kanada ve Meksika arasında bulunan, elli eyalet ve bir federal bölgeden oluşan, federal anayasal cumhuriyet ile yönetilen bir ülkedir."
     sonuc.classList.add("USA");
     document.getElementById("logo").setAttribute("src","amerika.png")
    }
    
    else if(ulke==3){
     document.body.style.backgroundColor="lightcoral"
-    sonucBaslık.innerHTML="Nüfus: 84,34 milyon"
+    sonucBaslık.innerHTML="Türkiye veya resmî adıyla Türkiye Cumhuriyeti, topraklarının büyük bölümü Anadolu'da, küçük bir bölümü ise Balkan Yarımadası'nın güneydoğu uzantısı olan Trakya'da yer alan bir ülkedir"
     sonuc.classList.add("TRY");
     document.getElementById("logo").setAttribute("src","Turkey.png")
    }
